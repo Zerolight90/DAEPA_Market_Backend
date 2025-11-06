a77129e5c133a4c539be314f642d471c0941f92e
Zerolight90 <ant7773@gmail.com>
2025-11-06 22:44:57 +0900

    판매중 판매완료 d.status 변경

--- src/main/java/com/daepamarket/daepa_market_backend/config/SecurityConfig.java
+++ src/main/java/com/daepamarket/daepa_market_backend/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
                 "http://localhost:3000",
                 "http://127.0.0.1:3000",
                 "http://192.168.*.*:3000",
+                "http://3.34.181.73/",
                 "*"
                 // 운영 도메인 추가
         ));
