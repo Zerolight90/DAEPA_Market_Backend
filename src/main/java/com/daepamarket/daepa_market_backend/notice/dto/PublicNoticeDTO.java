322bedf1701691f203b6312030a74a5f52433fc6
Zerolight90 <ant7773@gmail.com>
2025-11-11 00:06:16 +0900

    공지사항 게시판 작업

---
+++ src/main/java/com/daepamarket/daepa_market_backend/notice/dto/PublicNoticeDTO.java
@@ -0,0 +1,17 @@
+package com.daepamarket.daepa_market_backend.notice.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class PublicNoticeDTO {
+    private Long nIdx;
+    private String nSubject;
+    private String nContent;
+    private String adminNick;
+    private LocalDate nDate;
+    private String nImg;
+    private Byte nCategory; // Changed from nCategoryName to nCategory
+}
