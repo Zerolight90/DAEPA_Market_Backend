b1536cb235de34275d4a3074ed7e6f7e1c38e2a0
Zerolight90 <ant7773@gmail.com>
2026-03-07 00:22:48 +0900

    commit

--- src/main/java/com/daepamarket/daepa_market_backend/jwt/CookieProps.java
+++ src/main/java/com/daepamarket/daepa_market_backend/jwt/CookieProps.java
@@ -11,18 +11,18 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "app.cookie")
 public class CookieProps {
 
-    //쿠키를 허용할 도메인
+    // 쿠키를 허용할 도메인
     private String domain;
 
-    //HTTP 환경일 때 true
+    // HTTPS 환경이면 true
     private boolean secure;
 
-    //SameSite (배포단계에서는 None으로 바꾸기)
-    private String samesite = "Lax";
+    // SameSite: 크로스 도메인 배포이므로 None
+    private String samesite = "None";
 
-    //쿠키 기본 경로
+    // 쿠키 기본 경로
     private String path = "/";
 
-    //기본 쿠키 만료시간
+    // 기본 쿠키 만료시간
     private long defaultcookie = 3600;
 }
