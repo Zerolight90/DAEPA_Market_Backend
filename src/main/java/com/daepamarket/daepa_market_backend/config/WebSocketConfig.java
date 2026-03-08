9c9b1e95dec1a16d481a8e11b028d44f209efc86
Zerolight90 <ant7773@gmail.com>
2026-03-08 19:05:59 +0900

    commit

--- src/main/java/com/daepamarket/daepa_market_backend/config/WebSocketConfig.java
+++ src/main/java/com/daepamarket/daepa_market_backend/config/WebSocketConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,7 +14,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final StompAuthInterceptor stompAuthInterceptor;
+    private final ChannelInterceptor[] stompAuthInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
