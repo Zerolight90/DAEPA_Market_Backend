04f5b0ee3a53533012a0197ac04f053792133654
Zerolight90 <ant7773@gmail.com>
2026-03-10 13:29:04 +0900

    commit

--- src/main/java/com/daepamarket/daepa_market_backend/config/SecurityConfig.java
+++ src/main/java/com/daepamarket/daepa_market_backend/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
                 )
                 .oauth2Login(oauth -> oauth
                         .authorizationEndpoint(auth -> auth
-                                .baseUri("/api/oauth2/authorization"))
+                                .baseUri("/oauth2/authorization"))
                         .redirectionEndpoint(redir -> redir
                                 .baseUri("/api/login/oauth2/code/*"))
                         .userInfoEndpoint(userInfo ->
